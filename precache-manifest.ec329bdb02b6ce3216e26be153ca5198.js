6518d316f3243a4c755d8484f60a217b87b6c512
Kyle <kyle.rusby@gmail.com>
2020-08-05 19:05:25 +0100

    Updates

---
+++ precache-manifest.ec329bdb02b6ce3216e26be153ca5198.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c85db9903e0b58f97f0f7d3e692421e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e587df0d497731af23d2",
+    "url": "/static/css/2.b07ff35c.chunk.css"
+  },
+  {
+    "revision": "a12fe464ab3126e3f5ac",
+    "url": "/static/css/main.6615243d.chunk.css"
+  },
+  {
+    "revision": "e587df0d497731af23d2",
+    "url": "/static/js/2.ea820731.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ea820731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12fe464ab3126e3f5ac",
+    "url": "/static/js/main.26f95cf1.chunk.js"
+  },
+  {
+    "revision": "8bab28f5b3bffa57d3c1",
+    "url": "/static/js/runtime-main.2988248e.js"
+  }
+]);
